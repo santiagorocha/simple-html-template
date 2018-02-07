34ed755a036d6fc852ad9bd5202a7209eb0cf390
Andrey Miskov <amiskov@oggettoweb.com>
2018-02-07 03:58:32 +0300

    Add script for shirt colors

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,15 @@
 // Your code here...
+function Activew(){
+	document.getElementsByClassName("prod-s__img")[0].src = "img/tshirts/tshirt_white.jpg";
+}
+
+function Activey(){
+	document.getElementsByClassName("prod-s__img")[0].src = "img/tshirts/tshirt_yellow.jpg";
+}
+
+function Activeg(){
+	document.getElementsByClassName("prod-s__img")[0].src = "img/tshirts/tshirt_green.jpg";
+}
+function messagge(){
+    alert("Спасибо за покупки!");
+}
